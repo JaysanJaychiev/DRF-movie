e211a2a1cf37203c92172d45d571fe4d5633c5f6
JaysanJaychiev <jaysan.jaychiev.11@gmail.com>
2022-02-24 21:16:51 +0600

    allowed hosts

--- rest_movie/settings/production.py
+++ rest_movie/settings/production.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['my-cinema-api.herokuapp.com/']
+ALLOWED_HOSTS = ['my-cinema-api.herokuapp.com']
 
 
 # Application definition
